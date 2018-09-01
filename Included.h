2ae3586ab908c85afc4151d0e6c19b2aa1dacffc
Blackoutzz <mickael@sucuri.net>
2018-09-01 19:27:40 -0400

    Dependencies/Requirements | Updated!

--- Included.h
+++ Included.h
@@ -1,35 +1,33 @@
 #pragma once
-#define WIN32_LEAN_AND_MEAN   //Enleve lentete windows au fichier
+#define WIN32_LEAN_AND_MEAN
 #define _MAIN_H
 
-#pragma warning( disable : 4244 ) //remove the incoming warns
-#pragma warning( disable : 4996 ) //remove the incoming warns
+#pragma warning( disable : 4244 )
+#pragma warning( disable : 4996 )
 #pragma comment(lib, "d3d9.lib")
 #pragma comment(lib, "d3dx9.lib")
 #pragma comment(lib, "dwmapi.lib")
 #pragma comment(lib, "winmm.lib")
-#pragma comment(lib, "Detours.lib")
 #pragma comment(lib, "Shlwapi.lib")
 #pragma comment( lib, "gdiplus.lib" )
 
-#include <d3dx9.h>		// DirectX
-#include <d3d9.h>		// DirectX
-#include <Windows.h>	//WindowsAPi
-#include <iostream>		//IO
+#include <d3dx9.h>	
+#include <d3d9.h>	
+#include <Windows.h>
+#include <iostream>
 #include <dwmapi.h>		
-#include <ctime>		//Clock & Time Library
-#include <detours.h>	//Detour Microsoft Library
-#include <math.h>		//Math Library
-#include <vector>		//Math Vector Library
-#include <string>		//Strings
-#include <TlHelp32.h>	//Win32 Helper Library			
-#include <conio.h>		//IO		
-#include <gdiplus.h>	//GDI Microsoft Drawing Library	
-#include <stdio.h>		//standard IO
+#include <ctime>
+#include <math.h>
+#include <vector>
+#include <string>
+#include <TlHelp32.h>		
+#include <conio.h>
+#include <gdiplus.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <sstream>		//String Stream
+#include <sstream>
 #include <Psapi.h>	
-#include <process.h>	//Process Library
+#include <process.h>
 #include <Shlwapi.h>
 #include <regex>
 #include <string.h>
--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # CPP-Framework
 C++ Framework to build easy application and hack for Windows (Visual Studio).
 
+## Dependencies/Requirements
+- Microsoft DirectX SDK (June 2010) | For DirectX9
+
 ## Credits
 - Github : @Blackoutzz
 - Twitter : @Mick4Secure
